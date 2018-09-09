abf437a754ce5090d1e78f2841dc18bf2fc73925
CheataSo <cheataso1997@gmail.com>
2018-09-09 07:30:28 -0700

    create account design

--- app/src/main/java/com/example/ckcc/tosnham/MainActivity.java
+++ app/src/main/java/com/example/ckcc/tosnham/MainActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onStart() {
+        super.onStart();
         setContentView(R.layout.activity_main);
     }
+}
 }
