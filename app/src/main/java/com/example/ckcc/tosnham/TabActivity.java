3bdc87d9295b33b54e664561aa79ee5ab8d56b86
CheataSo <cheataso1997@gmail.com>
2018-09-18 20:24:04 +0700

    Merge branch 'master' of https://github.com/CheataSo/TosNham
    # Conflicts:
    #	app/src/main/res/layout/brown_page.xml
    #	app/src/main/res/values/colors.xml

---
+++ app/src/main/java/com/example/ckcc/tosnham/TabActivity.java
@@ -0,0 +1,22 @@
+package com.example.ckcc.tosnham;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by CKCC on 9/18/2018.
+ */
+
+public class TabActivity  extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+
+    }
+}
